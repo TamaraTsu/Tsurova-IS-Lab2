3020b72c6669d2d9baea33f1982a035593165684
Steven2110 <steven.wijaya2001@gmail.com>
2022-04-21 17:31:35 +0700

    Add app gui class and gitignore file

--- interface.py
+++ interface.py
@@ -1,54 +1,58 @@
-
-from textwrap import fill
 from tkinter import *
 
+class App:
+    def add_list(self):
+        for values in range(100):
+            self.listbox.insert(END, values)
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Genetic Algorithm")
+        self.window.geometry("500x300")
 
-window = Tk()
-window.title('PROGRAM')
-window.geometry("500x300")
+        # Left frame for information
+        self.frame = LabelFrame(self.window, width=200, height=200, bg="skyblue3")
+        self.frame.grid(row=0,column=0)
+        self.frame.propagate(0)
 
-
-frame = LabelFrame(window, width=200, height=200, bg="skyblue3")
-frame.grid(row=0,column=0)
-frame.propagate(0)
-
-label1 = Label(frame, text='The Best Genes:',
+        # All labels
+        self.label1 = Label(self.frame, text='The Best Genes:',
                background="skyblue3").pack(side=TOP, anchor="w")
-label2 = Label(frame, text='To be or not to be',
-               background="skyblue3").pack(pady=(0,10),side=TOP, anchor="w")
+        self.label2 = Label(self.frame, text='To be or not to be',
+                    background="skyblue3").pack(pady=(0,10),side=TOP, anchor="w")
 
-label3 = Label(frame, text='Generation' +
-               ' #35',
-               background="skyblue3").pack(side=TOP, anchor="w")
+        self.label3 = Label(self.frame, text='Generation' +
+                    ' #35',
+                    background="skyblue3").pack(side=TOP, anchor="w")
 
-label4 = Label(frame, text='Total Population' +
-               ' 600',
-               background="skyblue3").pack(side=TOP, anchor="w")
+        self.label4 = Label(self.frame, text='Total Population' +
+                    ' 600',
+                    background="skyblue3").pack(side=TOP, anchor="w")
 
-label5 = Label(frame, text='Average fitness :' +
-               '67.89%',
-               background="skyblue3").pack(side=TOP, anchor="w")
+        self.label5 = Label(self.frame, text='Average fitness :' +
+                    '67.89%',
+                    background="skyblue3").pack(side=TOP, anchor="w")
 
-label6 = Label(frame, text='Max fitness :' +
-               '100%',
-               background="skyblue3").pack(side=TOP, anchor="w")
+        self.label6 = Label(self.frame, text='Max fitness :' +
+                    '100%',
+                    background="skyblue3").pack(side=TOP, anchor="w")
 
-label7 = Label(frame, text='M... rate :' +
-               '2.0%',
-               background="skyblue3").pack(side=TOP, anchor="w")
+        self.label7 = Label(self.frame, text='Mutation rate :' +
+                    '2.0%',
+                    background="skyblue3").pack(side=TOP, anchor="w")
+        
+        # Right frame for list box
+        self.frame2 = LabelFrame(self.window, width=300, height=200, bg="skyblue2")
+        self.frame2.grid(row=0,column=1)
+        self.frame2.propagate(0)
 
-frame2 = LabelFrame(window, width=300, height=200, bg="skyblue2")
-frame2.grid(row=0,column=1)
-frame2.propagate(0)
+        self.scrollbar = Scrollbar(self.frame2,orient=VERTICAL)
 
-scrollbar = Scrollbar(frame2,orient=VERTICAL)
+        self.listbox = Listbox(self.frame2,yscrollcommand= self.scrollbar.set,width=290)
+        self.scrollbar.config(command= self.listbox.yview)
+        self.scrollbar.pack(side=RIGHT,fill=BOTH)
 
-listbox = Listbox(frame2,yscrollcommand= scrollbar.set,width=290)
-scrollbar.config(command= listbox.yview)
-scrollbar.pack(side=RIGHT,fill=BOTH)
+        self.listbox.pack(side=LEFT,fill=BOTH)
 
-listbox.pack(side=LEFT,fill=BOTH)
-for values in range(100):
-    listbox.insert(END, values)
-
-window.mainloop()
+        self.window.mainloop()
+        
+app = App()
